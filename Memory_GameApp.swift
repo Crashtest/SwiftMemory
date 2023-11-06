eee24d516abd0f27328ea6a8e64d4738624f868f
Trent Himes <crashtrent@yahoo.com>
2023-11-06 08:42:34 -0700

    Add files via upload

---
+++ Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Trent Himes on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
